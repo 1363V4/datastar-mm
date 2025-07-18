1e97857adcb0306594bec237ec63613c47983361
1363v4 <louisemmanuelgires@gmail.com>
2025-07-18 09:56:18 +0200

    dev

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ import asyncio
 from datetime import datetime, timedelta
 
 app = Sanic("mm")
-app.static('/static/', './static/')
+app.static("/static/", "./static/")
 app.static("/", "index.html", name="index")
+
+
+SURPRISE_HTML = """
+"""
+
 
 @app.get("/init")
 @datastar_response
@@ -17,8 +22,10 @@ async def init(request):
     while current_time < release:
         time_difference = release - current_time
         time_difference_str = str(timedelta(seconds=time_difference.seconds))
-        yield SSE.patch_elements(f'<main id="main"><div>{time_difference.days}d {time_difference_str}</div></main>')
+        yield SSE.patch_elements(
+            f'<main id="main"><div>{time_difference_str}</div></main>'
+        )
         await asyncio.sleep(1)
         current_time = datetime.now()
     else:
-        yield SSE.patch_elements(f'<main id="main"><div>???</div></main>')
+        yield SSE.patch_elements(SURPRISE_HTML)
